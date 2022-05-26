1247166fbff7dc7de800a7d7c41d4d61cb208593
Turki Jamaan <22967219+tjamaan@users.noreply.github.com>
2022-05-26 15:33:44 +0300

    scream if there is unparsed GltfExtras

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use bevy::prelude::*;
 use bevy::gltf::GltfExtras;
+use bevy::prelude::*;
 use bevy_inspector_egui::WorldInspectorPlugin;
 use serde_json::Value;
-
 
 fn main() {
     App::new()
@@ -16,22 +15,20 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin::new())
         .add_startup_system(setup_level1)
-        .add_system(parse_gltf_extras)
+        .add_system_to_stage(CoreStage::PreUpdate, parse_gltf_extras)
         .add_system(spinny_spin_spin)
+        .add_system(scream_and_shout)
         .run();
 }
 
-fn setup_level1(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-) {
+fn setup_level1(mut commands: Commands, asset_server: Res<AssetServer>) {
     // load and spawn scene
     commands.spawn_scene(asset_server.load("level1.glb#Scene0"));
 
     // setup perspective camera (transform x,y,z copied from blender)
     commands.spawn_bundle(PerspectiveCameraBundle {
         transform: Transform::from_xyz(7.35889, 6.92579, 4.95831)
-        .looking_at(Vec3::new(0.0,0.0,0.0), Vec3::Y),
+            .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
         ..default()
     });
 
@@ -48,16 +45,16 @@ fn parse_gltf_extras(
         // query every entity with GltfExtras
         (Entity, &GltfExtras),
         // filter only the newly added GltfExtras
-        Added<GltfExtras>>
+        Added<GltfExtras>,
+    >,
 ) {
     for (entity, gltf_extras) in query.iter() {
-        let v: Value = serde_json::from_str(&gltf_extras.value).expect("Couldn't parse GltfExtra value as JSON");
+        let v: Value = serde_json::from_str(&gltf_extras.value)
+            .expect("Couldn't parse GltfExtra value as JSON");
 
         if let Some(Value::Number(z_rot)) = v.get("z_rot") {
             let z_rot = z_rot.as_f64().expect("Invalid z_rot value");
-            commands
-                .entity(entity)
-                .insert(ZRot(z_rot as f32));
+            commands.entity(entity).insert(ZRot(z_rot as f32));
         }
     }
 }
@@ -65,11 +62,14 @@ fn parse_gltf_extras(
 #[derive(Component)]
 struct ZRot(f32);
 
-fn spinny_spin_spin(
-    mut query: Query<(&mut Transform, &ZRot)>,
-    time: Res<Time>
-) {
+fn spinny_spin_spin(mut query: Query<(&mut Transform, &ZRot)>, time: Res<Time>) {
     for (mut transform, z_rot) in query.iter_mut() {
         transform.rotate(Quat::from_rotation_z(z_rot.0 * time.delta_seconds()))
+    }
+}
+
+fn scream_and_shout(query: Query<(Entity), (With<GltfExtras>, Without<ZRot>)>) {
+    for entity in query.iter() {
+        println!("AAAaaaAAAaaA {:?}", entity);
     }
 }
